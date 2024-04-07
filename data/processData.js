5281398ed0e8113781e61de184faa3363e2929b9
yeoniii20 <commetoi_yeoni@naver.com>
2024-04-07 23:12:37 +0900

    feat: processData 분리

---
+++ data/processData.js
@@ -0,0 +1,47 @@
+export const TODO_LIST = [
+  {
+    title: "세수하기",
+    time: "10",
+    imagePath: require("../assets/img/action/washface.png"),
+  },
+  {
+    title: "양치하기",
+    time: "3",
+    imagePath: require("../assets/img/action/brushteeth.png"),
+  },
+  {
+    title: "식사하기",
+    time: "20",
+    imagePath: require("../assets/img/action/eat.png"),
+  },
+  {
+    title: "샤워하기",
+    time: "20",
+    imagePath: require("../assets/img/action/shower.png"),
+  },
+  {
+    title: "머리 말리고 손질하기",
+    time: "10",
+    imagePath: require("../assets/img/action/hair.png"),
+  },
+  {
+    title: "옷입기",
+    time: "2",
+    imagePath: require("../assets/img/action/clothing.png"),
+  },
+  {
+    title: "화장하기",
+    time: "15",
+    imagePath: require("../assets/img/action/makeup.png"),
+  },
+  {
+    title: "짐 챙기기",
+    time: "5",
+    imagePath: require("../assets/img/action/bag.png"),
+  },
+  {
+    title: "여유 부리기",
+    time: "10",
+    imagePath: require("../assets/img/action/procrastinate.png"),
+  },
+];
