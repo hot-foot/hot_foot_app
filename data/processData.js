d2d6d7583e84b9cf2b9c68a25d6b17c7409fbefd
yeoniii20 <commetoi_yeoni@naver.com>
2024-04-10 02:00:03 +0900

    update: name 추가

--- data/processData.js
+++ data/processData.js
@@ -49,62 +49,77 @@ export const TODO_LIST = [
 export const TODO_ICON = [
   {
     id: 1,
+    name: "BB1",
     imagePath: require("../assets/img/action/BB1.png"),
   },
   {
     id: 2,
+    name: "BB2",
     imagePath: require("../assets/img/action/BB2.png"),
   },
   {
     id: 3,
+    name: "BB3",
     imagePath: require("../assets/img/action/BB3.png"),
   },
   {
     id: 4,
+    name: "BB4",
     imagePath: require("../assets/img/action/BB4.png"),
   },
   {
     id: 5,
+    name: "BB5",
     imagePath: require("../assets/img/action/BB5.png"),
   },
   {
     id: 6,
+    name: "BB6",
     imagePath: require("../assets/img/action/BB6.png"),
   },
   {
     id: 7,
+    name: "washface",
     imagePath: require("../assets/img/action/washface.png"),
   },
   {
     id: 8,
+    name: "brushteeth",
     imagePath: require("../assets/img/action/brushteeth.png"),
   },
   {
     id: 9,
+    name: "eat",
     imagePath: require("../assets/img/action/eat.png"),
   },
   {
     id: 10,
+    name: "shower",
     imagePath: require("../assets/img/action/shower.png"),
   },
   {
     id: 11,
+    name: "hair",
     imagePath: require("../assets/img/action/hair.png"),
   },
   {
     id: 12,
+    name: "clothing",
     imagePath: require("../assets/img/action/clothing.png"),
   },
   {
     id: 13,
+    name: "makeup",
     imagePath: require("../assets/img/action/makeup.png"),
   },
   {
     id: 14,
+    name: "bag",
     imagePath: require("../assets/img/action/bag.png"),
   },
   {
     id: 15,
+    name: "procrastinate",
     imagePath: require("../assets/img/action/procrastinate.png"),
   },
 ];
